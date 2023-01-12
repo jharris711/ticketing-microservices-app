63a1828fe2168542481252fb53f767238e3cbdac
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-12 00:26:36 -0500

    Test route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(json());
 
+app.get(`/api/users/currentuser`, (req, res) => {
+  res.send(`Hi there!`);
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
