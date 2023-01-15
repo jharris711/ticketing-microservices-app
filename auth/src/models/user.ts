3adb156c6dc04e3afabc7dd061b141307ea4a3b0
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-14 22:38:50 -0500

    Password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services';
 
 /**
  * Describes the propeties that are required to
@@ -33,6 +34,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+// Handle password
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 // Add method to the schema
