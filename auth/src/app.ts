bbad58cd7b40785266817052e252b88d9fd8ea69
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-17 10:33:10 -0500

    index -> app refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,45 @@
+import express, { json } from 'express';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+import { errorHandler } from './middleware';
+import {
+  signinRouter,
+  signoutRouter,
+  signupRouter,
+  currentUserRouter,
+} from './routes';
+import { NotFoundError } from './errors';
+
+/**
+ * App setup
+ */
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+/**
+ * Routes
+ */
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all(`*`, async (req, res) => {
+  throw new NotFoundError();
+});
+
+/**
+ * Error Handler
+ */
+app.use(errorHandler);
+
+export default app;
