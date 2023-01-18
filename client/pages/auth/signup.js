52b0ebb891f33b7ee21a1732b338c3766127789b
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-18 14:04:29 -0500

    Form handlers

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Signup() {
+  const [email, setEmail] = useState(``);
+  const [password, setPassword] = useState(``);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(`${email}: ${password}`);
+  };
+
   return (
     <div className='p-2'>
-      <form action=''>
+      <form onSubmit={onSubmit}>
         <h1>Sign Up</h1>
         <div className='form-group m-1'>
           <label>Email Address</label>
-          <input type='email' className='form-control' />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type='email'
+            className='form-control'
+          />
         </div>
         <div className='form-group m-1'>
           <label>Password</label>
-          <input type='password' className='form-control' />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+            className='form-control'
+          />
         </div>
         <button className='btn btn-primary m-1'>Sign Up</button>
       </form>
