5214a852ae21c07994f6f8cecb229709a77c816f
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-18 15:20:48 -0500

    useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,28 +1,40 @@
 import React, { useState } from 'react';
+import Router from 'next/router';
+import { useRequest } from '../../hooks';
 
 function Signup() {
   const [email, setEmail] = useState(``);
   const [password, setPassword] = useState(``);
+  const { doRequest, errors } = useRequest({
+    url: `/api/users/signup`,
+    method: `post`,
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push(`/`),
+  });
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    console.log(`${email}: ${password}`);
+
+    doRequest();
   };
 
   return (
     <div className='p-2'>
       <form onSubmit={onSubmit}>
         <h1>Sign Up</h1>
-        <div className='form-group m-1'>
+        <div className='form-group my-1'>
           <label>Email Address</label>
           <input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            type='email'
+            /* type='email' */
             className='form-control'
           />
         </div>
-        <div className='form-group m-1'>
+        <div className='form-group my-1'>
           <label>Password</label>
           <input
             value={password}
@@ -31,7 +43,8 @@ function Signup() {
             className='form-control'
           />
         </div>
-        <button className='btn btn-primary m-1'>Sign Up</button>
+        {errors}
+        <button className='btn btn-primary my-1'>Sign Up</button>
       </form>
     </div>
   );
