51e51473c3e78b986a7a8681d9a6b84fd8a49a00
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-20 13:45:56 -0500

    Ticket creation via route / Test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import app from '../../app';
+import { Ticket } from '../../models';
 
 it(`has a route handler listening to /api/tickets for post requests`, async () => {
   const response = await request(app).post(`/api/tickets`).send({});
@@ -55,11 +56,22 @@ it(`returns an error if an invalid price is provided`, async () => {
 
 it(`creates a ticket with valid inputs`, async () => {
   // Add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = `Ticket Title`;
+
   await request(app)
     .post(`/api/tickets`)
+    .set(`Cookie`, global.signin())
     .send({
-      title: `Ticket Title`,
+      title,
       price: 20,
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,21 +1,35 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { validateRequests, requireAuth } from '@jheezytix/common';
+import { Ticket } from '../models';
 
 const router = express.Router();
+
+const titleValidator = body(`title`)
+  .not()
+  .isEmpty()
+  .withMessage(`Title is required`);
+const priceValidator = body(`price`)
+  .isFloat({ gt: 0 })
+  .withMessage(`Price must be greater than zero`);
 
 router.post(
   `/api/tickets`,
   requireAuth,
-  [
-    body(`title`).not().isEmpty().withMessage(`Title is required`),
-    body(`price`)
-      .isFloat({ gt: 0 })
-      .withMessage(`Price must be greater than zero`),
-  ],
+  [titleValidator, priceValidator],
   validateRequests,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
