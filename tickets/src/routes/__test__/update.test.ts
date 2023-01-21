23f8d2332dfa6957d3bc502e0c8a03ba375f66be
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-01-21 13:08:29 -0500

    Update ticket route

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -35,6 +35,56 @@ it(`return a 401 if the user does not own the ticket`, async () => {
     .expect(401);
 });
 
-it(`return a 400 if the user provides an invalid title or price`, async () => {});
+it(`return a 400 if the user provides an invalid title or price`, async () => {
+  // Use same user for both reqs
+  const cookie = global.signin();
 
-it(`updates the ticket provided valid inputs`, async () => {});
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set(`Cookie`, cookie)
+    .send({
+      title: `aljkdsf`,
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set(`Cookie`, cookie)
+    .send({ title: ``, price: 20 })
+    .expect(400);
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set(`Cookie`, cookie)
+    .send({ title: `valid title`, price: -20 })
+    .expect(400);
+});
+
+it(`updates the ticket provided valid inputs`, async () => {
+  // Use same user for both reqs
+  const cookie = global.signin();
+
+  // Create a ticket
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set(`Cookie`, cookie)
+    .send({
+      title: `aljkdsf`,
+      price: 20,
+    })
+    .expect(201);
+
+  // Send an update
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set(`Cookie`, cookie)
+    .send({ title: `Valid Title`, price: 100 })
+    .expect(200);
+
+  // Get the updated ticket for equality checking
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual(`Valid Title`);
+  expect(ticketResponse.body.price).toEqual(100);
+});
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -10,17 +10,32 @@ import { Ticket } from '../models';
 
 const router = express.Router();
 
+const validators = [
+  body(`title`).not().isEmpty().withMessage(`Title is required`),
+  body(`price`)
+    .isFloat({ gt: 0 })
+    .withMessage(`Price must be provided and must be greater than 0`),
+];
+
 router.put(
   `/api/tickets/:id`,
   requireAuth,
+  validators,
+  validateRequests,
   async (req: Request, res: Response) => {
+    // Check for ticket
     const ticket = await Ticket.findById(req.params.id);
-
     if (!ticket) throw new NotFoundError();
 
-    if (ticket.userId !== req.currentUser!.id) {
-      throw new NotAuthorizedError();
-    }
+    // Make sure use owns ticket before applying updates
+    const notAuthorized = ticket.userId !== req.currentUser!.id;
+    if (notAuthorized) throw new NotAuthorizedError();
+
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
 
     res.send(ticket);
   }
