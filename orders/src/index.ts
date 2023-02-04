ae4a615d67deb61f3d93a6ab087ea3690d8fd3c5
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-02-03 21:08:57 -0500

    comment

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -16,6 +16,7 @@ const natsUrl = process.env.NATS_URL;
 const natsClusterId = process.env.NATS_CLUSTER_URL;
 
 const start = async () => {
+  console.log('Starting orders service...');
   if (!jwtKey) {
     throw new Error('JWT key must be defined');
   }
